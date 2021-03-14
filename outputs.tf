5f534e045b784d6180d3468732628af4ecd818e7
watcher00090 <james00090@gmail.com>
2021-03-14 15:34:27 -0700

    now outputting all the masters ips in a single list also

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-//output "instance_info" {  
-//    value = module.workshop.*.master_ips
-//}
+output "instance_info" {  
+    value = [for i in range(0,length(module.workshop)): module.workshop[i].master_ip]
+}
 
 output "instance_ips" {
     value = [for i in range(0,length(module.workshop)): 
